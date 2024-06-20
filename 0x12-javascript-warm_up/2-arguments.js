9e4b9737d05fe8b0e6090bd86bae76924aa885e1
Dennis Miring'u <markdennis05771@gmail.com>
2024-06-20 01:39:20 -0700

    all

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = process.argv.slice(2);
+
+if (arg.length === 0){
+	console.log("No arguement");
+}else if (arg.length === 1){
+	console.log("Arguement found");
+}else {
+	consol.log("Arguements found");
+}
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,2 @@
+JavaScript introdustion
+Dennis Miring'u
